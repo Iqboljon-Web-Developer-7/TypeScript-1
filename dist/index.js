9e5106d4b1c52d7cd96b6e94d326ec00707380d7
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-09-27 11:40:25 +0500

    updated

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-let a = "Anor";
+// 1 - misol
+let ism = "John Doe";
+let age = 30;
+let isStudent = true;
+let hobbies = ["Reading", "Gaming", "Coding"];
+function greet(person) {
+    return `Hello, ${person}`;
+}
+function calculateSum(a, b) {
+    return a + b;
+}
+function isAdult(personAge) {
+    return personAge >= 18;
+}
--- root/index.ts
+++ root/index.ts
@@ -1,1 +1,17 @@
-let a = "Anor";
+// 1 - misol
+let ism: String = "John Doe";
+let age: Number = 30;
+let isStudent: Boolean = true;
+let hobbies: String[] = ["Reading", "Gaming", "Coding"];
+
+function greet(person: String): String {
+  return `Hello, ${person}`;
+}
+
+function calculateSum(a: number, b: number): number {
+  return a + b;
+}
+
+function isAdult(personAge: number): boolean {
+  return personAge >= 18;
+}
