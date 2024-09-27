0d10f3f8579d6a7cb51e8d34d18dfee03ee908a2
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-09-27 12:07:56 +0500

    updated

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,33 @@ let age = 30;
 let isStudent = true;
 let hobbies = ["Reading", "Gaming", "Coding"];
 function greet(person) {
-    return `Hello, ${person}`;
+  return `Hello, ${person}`;
 }
 function calculateSum(a, b) {
-    return a + b;
+  return a + b;
 }
 function isAdult(personAge) {
-    return personAge >= 18;
+  return personAge >= 18;
 }
+// 2 - misol
+function countPositiveNumbers(arr) {
+  let res = 0;
+  arr.forEach((item) => {
+    item >= 0 ? (res += 1) : null;
+  });
+  console.log(res);
+}
+// countPositiveNumbers([1, 2, 3]);
+// 3 - misol
+function fizzBuzz(n) {
+  for (let i = 0; i < n; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    }
+  }
+}
+fizzBuzz(15);
--- root/index.ts
+++ root/index.ts
@@ -1,7 +1,7 @@
 // 1 - misol
-let ism: String = "John Doe";
-let age: Number = 30;
-let isStudent: Boolean = true;
+let ism: string = "John Doe";
+let age: number = 30;
+let isStudent: boolean = true;
 let hobbies: String[] = ["Reading", "Gaming", "Coding"];
 
 function greet(person: String): String {
@@ -15,3 +15,28 @@ function calculateSum(a: number, b: number): number {
 function isAdult(personAge: number): boolean {
   return personAge >= 18;
 }
+
+// 2 - misol
+function countPositiveNumbers(arr: number[]) {
+  let res: number = 0;
+  arr.forEach((item: number): void => {
+    item >= 0 ? (res += 1) : null;
+  });
+  console.log(res);
+}
+
+// countPositiveNumbers([1, 2, 3]);
+
+// 3 - misol
+function fizzBuzz(n: number): void {
+  for (let i = 0; i < n; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else if (i % 3 == 0) {
+      console.log("FizzBuzz");
+    }
+  }
+}
+fizzBuzz(15);
